a7effd910390d8081193ca214475ba78524cca4c
84R5 <o.u.n@yandex.ru>
2023-04-11 22:37:24 +0300

    <add> constant for recommendation method

--- src/main/java/ru/yandex/practicum/filmorate/model/Constants.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Constants.java
@@ -5,4 +5,9 @@ import java.time.LocalDate;
 public class Constants {
     public static final int MAX_LENGTH_DESCRIPTION_FILM = 200;
     public static final LocalDate CINEMA_BIRTHDAY = LocalDate.of(1895, 12, 28);
+
+
+    public static final int FILM_RATE_DELTA = 1;
+    public static final int FILM_RATE_LO = 1;
+    public static final int FILM_RATE_AV = 6;
 }
